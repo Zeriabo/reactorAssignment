da3b59e0974581e8a95988b44be4acbdabafd204
Zeriab Chiah <35579766+Zeriabo@users.noreply.github.com>
2020-11-13 15:14:36 +0200

    Add files via upload

---
+++ build/precache-manifest.f03e37c457a226b178b9778e616ef156.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8bd2005b9bb0d17a411abe28ccd380",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9e66fc2d1cf9b969ea53",
+    "url": "./static/css/2.1d7ff099.chunk.css"
+  },
+  {
+    "revision": "8b60ab15eba35ff947cd",
+    "url": "./static/css/main.516dfad8.chunk.css"
+  },
+  {
+    "revision": "9e66fc2d1cf9b969ea53",
+    "url": "./static/js/2.2c475707.chunk.js"
+  },
+  {
+    "revision": "9c7d1755de68e17ad3489603d3c40b55",
+    "url": "./static/js/2.2c475707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b60ab15eba35ff947cd",
+    "url": "./static/js/main.f893383d.chunk.js"
+  },
+  {
+    "revision": "2746caf1b8c3bdc13657",
+    "url": "./static/js/runtime-main.caa8b5f7.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "./static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "./static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "./static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "./static/media/notification.c0d3c94c.eot"
+  }
+]);
